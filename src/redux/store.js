4a6c6b734f8cef7a90b3b293d1998d38b96eb475
Veres Adam <veresadam2002@gmail.com>
2021-08-15 17:17:44 +0300

    implemented logging just for development

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,8 @@
-import { createStore, applyMiddleware } from 'redux';
-import { persistStore } from 'redux-persist';
+import { applyMiddleware, createStore } from 'redux';
 import logger from 'redux-logger';
-
+import { persistStore } from 'redux-persist';
 import rootReducer from './root-reducer';
+
 
 const middlewares = [];
 
